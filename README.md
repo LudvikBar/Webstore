b199d59b4d33075c0bc4519070a5cc1e4a9422ab
Ludvik Skarstad Barlaug <ludvik@MacBook-Pro.local>
2026-04-27 14:15:46 +0200

    signin and signup

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Webstore
 <br>
 
-### In this project I will create a webstore, with auth, and the normal webstore properties.
+### In this project I will create a webstore, with auth, and the normal webstore features.
+
+
+The project will utilize fetch, POST, express + bcrypt and MySQL/MariaDB.
+
+<br>
+<br>
+Setup guide will come
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const path = require('path');
+
+
+
+
+
+const app = express(); /* starts express server, this is like turning on the server*/
+
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+/*
+app.use() means "run this on every request".
+
+express.json() — lets your server read the JSON data sent 
+from your HTML fetch calls. Without this, req.body would be empty
+
+express.static(...) — serves everything in your public/ folder 
+automatically. This is how your HTML and CSS files 
+get sent to the browser
+*/
